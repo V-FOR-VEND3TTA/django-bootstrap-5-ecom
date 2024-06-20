05ceefefc8375804922e5c5f9428873bade97ce0
George Hlongwane <georgehlongwane8@gmail.com>
2024-06-20 09:27:34 +0200

    Created models for category, product, order

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
